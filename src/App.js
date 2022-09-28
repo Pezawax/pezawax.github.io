37eea1b3e489c0be2fd5ca58cc49090e2987d28f
Mehmet Fatih <okuyanf6161@gmail.com>
2022-09-29 00:44:13 +0300

    feat: add website

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
+import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import "./App.css";
+
+class App extends React.Component {
+    titleStyle = {
+        fontSize: "15vw",
+    };
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+          </div>
+          <div className="social">
+            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+              <IconFacebook className="icon" />
+            </a>
+            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
+              <IconTwitter className="icon" />
+            </a>
+            <a href="https://github.com/pezawax" title="GitHub" target="_blank" rel="noopener noreferrer">
+              <IconGithub className="icon" />
+            </a>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1 style={this.titleStyle}>PEZAWAX</h1>
+          </div>
+        </div>
+        <div className="footer">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
